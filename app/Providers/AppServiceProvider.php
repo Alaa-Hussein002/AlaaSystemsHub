d0cebe1239262e85a9d163d911ceb8845b66b62a
Alaa-Hussein002 <ala.hussein002@gmail.com>
2026-07-23 15:08:22 +0300

    feat: enforce HTTPS scheme in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\Sanctum;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,5 +16,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
