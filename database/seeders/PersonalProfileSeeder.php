358ce07537ad01edee6e4d6a2886be9753723a85
Alaa-Hussein002 <ala.hussein002@gmail.com>
2026-07-24 01:01:16 +0300

    fix: update personal profile seeder with correct name and contact details

--- database/seeders/PersonalProfileSeeder.php
+++ database/seeders/PersonalProfileSeeder.php
@@ -13,21 +13,13 @@ class PersonalProfileSeeder extends Seeder
         PersonalProfile::updateOrCreate(
             ['id' => 1],
             [
-                'full_name' => ['ar' => 'علاء الدين', 'en' => 'Alaa Eddin'],
+                'full_name' => ['ar' => 'علاء حسين', 'en' => 'Alaa Hussein'],
                 // 'title' => ['ar' => 'مطور ويب متكامل', 'en' => 'Full Stack Developer'],
-                'bio' => [
-                    'ar' => 'مطور ويب محترف مع خبرة في بناء تطبيقات حديثة',
-                    'en' => 'Professional web developer with experience in building modern applications'
-                ],
                 'available_for_hire' => true,
                 'is_published' => true,
                 'contact' => [
-                    'email' => 'contact@alaasystems.com',
-                    'phone' => '+967771234567'
-                ],
-                'social_links' => [
-                    'github' => 'https://github.com/username',
-                    'linkedin' => 'https://linkedin.com/in/username',
+                    'email' => 'ala.hussein002@gmail.com',
+                    'phone' => '+966509651996'
                 ],
             ]
         );
